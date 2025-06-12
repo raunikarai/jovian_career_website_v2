5be247164a604c251352aa45a1988ebd59f800b6
Raunika Rai <raunikarai08@gmail.com>
2025-06-12 17:19:23 +0530

    added HTML and CSS

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask 
+from flask import Flask,render_template #importing Flask class from class library
 app=Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return " hello, tiya "
+def hello_world(): #running this fuction whenver some one open the browser 
+    return render_template('home.html')
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", debug=True)
