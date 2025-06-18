5b07666bdc37960798b2a2666182dd8fc6c809bb
Raunika Rai <raunikarai08@gmail.com>
2025-06-18 18:07:08 +0530

    added connection to db

--- app.py
+++ app.py
@@ -1,39 +1,17 @@
 from flask import Flask,render_template, jsonify#importing Flask class from class library
+from database import load_jobs_from_db
 app=Flask(__name__)
-
-JOBS=[{
-    'id':1,
-    'title':'Data Analyst',
-    'Location':'Bengluru,India',
-    'Salary':'10,00,000',
-    },
-{
-    'id':2,
-    'title':'Data Scientist',
-    'Location':'Bengluru,India',
-    'Salary':'15,00,000',
-},
-{
-    'id':3,
-    'title':'Frontend Engineer',
-    'Location':'Delhi,India',
-    'Salary':'1,00,000',
-},
-{
-    'id':4,
-    'title':'Backend Engineer',
-    'Location':'Chennai,India',
-    'Salary':'1,00,000',
-}]
-
-
+                 
 @app.route("/")
 def hello_world(): #running this fuction whenver some one open the browser 
-    return render_template('home.html',jobs=JOBS, company_name='Jovian')
+    jobs=load_jobs_from_db()
+    return render_template('home.html',jobs=jobs, company_name='Jovian')
 
 @app.route("/jobs")
 def list_jobs():
-    return jsonify(JOBS) #my web server is not only returing a HTML page but also returning the information in the for of json 
+    jobs = load_jobs_from_db()
+    return jsonify(jobs) 
+#my web server is not only returing a HTML page but also returning the information in the for of json 
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", debug=True)
---
+++ database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine, text
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load variables from .env
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(db_connection_string)
+
+def load_jobs_from_db():
+    with engine.connect() as conn:
+     result=conn.execute(text("select * from JOBS"))
+     jobs=[]
+    for row in result.all():
+         jobs.append(dict(row._mapping))
+    return jobs
+        
+        
