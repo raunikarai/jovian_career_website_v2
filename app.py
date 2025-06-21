2365c045897c14b00cc80728b2fd82b3934ad3aa
Raunika Rai <raunikarai08@gmail.com>
2025-06-21 16:56:22 +0530

    dynamic pages for each job

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import Flask,render_template, jsonify#importing Flask class from class library
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 app=Flask(__name__)
                  
 @app.route("/")
 def hello_world(): #running this fuction whenver some one open the browser 
     jobs=load_jobs_from_db()
-    return render_template('home.html',jobs=jobs, company_name='Jovian')
+    return render_template('home.html',jobs=jobs)
 
 @app.route("/jobs")
 def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs) 
 #my web server is not only returing a HTML page but also returning the information in the for of json 
+
+@app.route("/jobs/<id>")
+def show_job(id):
+    job=load_job_from_db(id)
+    if not job:
+        return "Not found", 404
+    return render_template('jobpage.html',job=job)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", debug=True)
--- database.py
+++ database.py
@@ -17,3 +17,13 @@ def load_jobs_from_db():
     return jobs
         
         
+def load_job_from_db(id):
+     with engine.connect() as conn:
+          result=conn.execute(text("select * from JOBS where ID = :val"),{"val": id})
+          row = result.fetchone()  # Fetch just one row
+
+     if row is None:
+        return None
+     else:
+        return dict(row._mapping) 
+     
